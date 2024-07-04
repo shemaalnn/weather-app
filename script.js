649818cb1c99d5787795e0c876a028e37ab18277
Shema <shema.aln@outlook.com>
2024-07-04 23:39:13 +0100

    functionality worked basic styling done

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ function searchWeather() {
     const locationName = cityInputEl.value.trim();
     if (locationName) {
         getCurrentWeather(locationName);
+        getWeatherForecast(locationName)
         saveLocation(locationName);
     } else {
         alert("Please enter a location to see the weather");
@@ -33,7 +34,7 @@ function searchWeather() {
 }
 
 function getCurrentWeather(locationName) {
-    var requestUrl =`${WEATHER_API_BASE_URL}/weather?q=${locationName}&units=metric&appid=${WEATHER_API_KEY}`
+    var requestUrl =`${WEATHER_API_BASE_URL}/data/2.5/weather?q=${locationName}&units=metric&appid=${WEATHER_API_KEY}`
     fetch(requestUrl)
        .then(response => {
             if (!response.ok) {
@@ -50,14 +51,79 @@ function getCurrentWeather(locationName) {
             alert('Location not found. Please try again');
        });
 }
+
+// Fetching the 5-Day weather forecast 
+function getWeatherForecast(locationName) {
+    var requestUrl = `${WEATHER_API_BASE_URL}/data/2.5/forecast?q=${locationName}&units=metric&appid=${WEATHER_API_KEY}`;
+    fetch (requestUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Location not found!");
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayWeatherForecast(data);
+        })
+        .catch(error => {
+            console.error('Error fetching forecast:', error);
+        })
+}
 // Displaying current weather 
 function displayCurrentWeather(data) {
     currentLocationEl.textContent = data.name 
     tempEl.textContent = `${data.main.temp}°C`;
     humidityEl.textContent = `${data.main.humidity}%`;
     windEl.textContent = `${data.wind.speed} m/s`;
-    weatherIconEl.querySelector('img').src=`https://openweathermap.org/img/wn/10d@2x.png"`;
+    weatherIconEl.querySelector('img').src=`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
     weatherEl.style.display = "block";
+}
+
+// Displaying the weather forecast 
+function displayWeatherForecast(data) {
+    forecastEl.style.display = "block";
+    forecastDaysEl.innerHTML = '';
+
+    const dailyForecast = data.list.filter(entry => entry.dt_txt.includes("12:00:00")).slice(0, MAX_DAILY_FORECAST);
+
+    dailyForecast.forEach(forecast  => {
+        const forecastEl = document.createElement('div');
+        forecastEl.classList.add('forecast-day');
+
+        const dateEl = document.createElement('div');
+        dateEl.classList.add('date');
+        dateEl.textContent = formatDate(forecast.dt_txt);
+
+        const iconEl = document.createElement('img');
+        iconEl.classList.add('forecast-icon');
+        iconEl.src = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`;
+        iconEl.alt = forecast.weather[0].description;
+
+        const tempEl = document.createElement('div');
+        tempEl.classList.add('temp');
+        tempEl.textContent = `Temp: ${forecast.main.temp}°C`
+
+        const windEl = document.createElement('div');
+        windEl.classList.add('wind');
+        windEl.textContent = `Wind: ${forecast.wind.speed} m/s`;
+
+        const humidityEl = document.createElement('div');
+        humidityEl.classList.add('humidity');
+        humidityEl.textContent = `Humidity: ${forecast.main.humidity}%`;
+
+        forecastEl.appendChild(dateEl);
+        forecastEl.appendChild(iconEl);
+        forecastEl.appendChild(tempEl);
+        forecastEl.appendChild(windEl);
+        forecastEl.appendChild(humidityEl);
+
+        forecastDaysEl.appendChild(forecastEl);
+    })
+}
+
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', { weekday: 'short' });
 }
 
 //Saving the locations to local storage 
@@ -94,3 +160,6 @@ clearSearchEl.addEventListener('click', () => {
 
 //an event listener for the search button to fetch the data when clicked 
 searchButtonEl.addEventListener('click', searchWeather)
+
+// initial call to display past locations when page loads 
+displayPastLocations();
